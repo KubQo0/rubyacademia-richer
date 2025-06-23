1cf3ada11470dd478c6ba96395ed52d22e30acf4
jakub danis <jakub13.danis@gmail.com>
2025-06-23 14:34:49 +0200

    implemented devise

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, only: :secret
   def home
+  end
+
+  def secret
+    #redirect_to root_path unless user_signed_in?
   end
 end
