3abffd00bdf95b3de4f38d17ef69d0b405071f63
KubQo0 <75166030+KubQo0@users.noreply.github.com>
2025-06-24 13:05:08 +0200

    unfinished translation (#4)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get "secret", to: "pages#secret"
-  devise_for :users
+  scope "(:locale)" do
+    resources :transactions
+    devise_for :users
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -12,5 +15,6 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
+  get ":locale", to: "pages#home", as: :root_with_locale
   root "pages#home"
 end
