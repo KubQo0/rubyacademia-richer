0f796f46892ea7df8c00f9a31b332c7cbcad9b53
jakub danis <jakub13.danis@gmail.com>
2025-06-24 10:18:19 +0200

    added index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_06_23_120950) do
+ActiveRecord::Schema[7.2].define(version: 2025_06_23_165210) do
+  create_table "transactions", force: :cascade do |t|
+    t.string "title"
+    t.integer "kind", default: 0, null: false
+    t.float "amount", default: 0.0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_transactions_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -20,4 +30,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_06_23_120950) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
+  add_foreign_key "transactions", "users"
 end
